8a9249b2c5b39dc23dc7c0ae8d6000fec20e3f6c
kyle-mcknight <kyle.mcknight@accesso.com>
2020-06-16 17:34:05 -0400

    Adding response from socket

--- socket-server/app.js
+++ socket-server/app.js
@@ -6,8 +6,15 @@ server.listen(8082);
 
 io.on('connection', (socket) => {
 	console.log('connected on server');
-	socket.emit('connected', { hello: 'world' });
+
+	socket.emit('connected');
+
 	socket.on('received', (data) => {
 		console.log(data);
 	});
+
+	socket.on('dice-roll', (callbackFn) => {
+		console.log(`Socket received request for dice-roll`);
+		callbackFn({prop1: 'prop1', prop2: 'prop2'});
+	})
 });
--- web-endpoint/routes/index.js
+++ web-endpoint/routes/index.js
@@ -1,10 +1,15 @@
+const socket = require('../app').socket;
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  console.log('HTTP received request')
   res.set('Access-Control-Allow-Origin', '*');
-  res.render('index', { title: 'Express' });
+
+  const socketResponse = await new Promise(resolve => socket.emit('dice-roll', socketResponse => resolve(socketResponse)));
+  console.log(`Received response from socket ${socketResponse}`);
+  res.send(socketResponse);
 });
 
 module.exports = router;
