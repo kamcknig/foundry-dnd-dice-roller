0ff60fb756079f5c4eeee545e73d1d650933c4bb
Ubuntu <ubuntu@ip-172-31-58-244.ec2.internal>
2020-06-24 00:52:37 +0000

    Removing web server and updated server to allow for mobile connection direclty

--- socket-server/app.js
+++ socket-server/app.js
@@ -1,27 +1,52 @@
 const io = require('socket.io')(8082);
 
 // temporary variable
-let foundrySocket;
-
-/**
- * Create the /foundry namespace. This is where sockets connect from the Foundry game add-on
- */
-const foundryNsp = io.of('/foundry');
-foundryNsp.on('connection', socket  => {
-	console.log(`Socket server - client '${socket.id}' connected to foundry namespace`);
-	foundrySocket = socket;
-});
+let connections = new Map();
 
 /**
  * Create the default namespace. This is where the HTTP server connects
  */
 io.on('connection', (socket) => {
 	console.log(`Socket server - client '${socket.id}' connected to default namespace`);
+	connections.set(socket.id, socket);
 
-	socket.on('dice-roll', async (callbackFn) => {
-		console.log(`Socket server - received request for 'dice-roll'`);
-		const addOnResponse = await new Promise(resolve => foundrySocket.emit('addon-dice-roll', result => resolve(result)));
-		console.log(`Socket server received roll response ${addOnResponse}`);
-		callbackFn(addOnResponse);
-	})
+	socket.on('disconnect', reason => {
+		console.log(`Socket server - '${socket.id}' disconnected from default namesapce`);
+		console.log(`Reason '${reason}'`);
+		connections.delete(socket.id);
+	});
+
+	socket.on('request-roll', async callbackFn => {
+		console.log(`Socket server - Received request for dice roll from '${socket.id}'`);
+
+		// right now it loops through and emits to all foundry instances. Which while testing is simply 
+		// one instance. need to be able to 'connect' the phone app to the identity in game
+		const result = await new Promise(async resolve => {
+			let result = 0;
+			for(const [id, socket] of foundryConnections.entries()) {
+				console.log(`Socket server - emitting 'foundry-roll' to foundry namespace connections`);
+				result = await new Promise(resolve => socket.emit('foundry-roll', result => resolve(result)));
+			};
+
+			resolve(result);
+		});
+
+		console.log(`Socket server - Received result from foundry`);
+		console.log(result);
+		callbackFn(typeof result === 'object' ? result : { result: { total: result } });
+	});
+});
+
+let foundryConnections = new Map();
+
+const foundryNsp = io.of('/foundry');
+foundryNsp.on('connection', socket => {
+	console.log(`Socket server - foundry app '${socket.id}' has connected to server`);
+	foundryConnections.set(socket.id, socket);
+
+	socket.on('disconnect', reason => {
+		console.log(`Socket server - foundry app '${socket.id}' has disconnected from server`);
+		console.log(`${reason}`);
+		foundryConnections.delete(socket.id);
+	});
 });
