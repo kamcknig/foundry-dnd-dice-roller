ca99be19420a308d37008539bd7b2320cb196c43
kyle-mcknight <kyle.mcknight@accesso.com>
2020-06-26 15:53:29 -0400

    yep

--- socket-server/app.js
+++ socket-server/app.js
@@ -3,7 +3,6 @@ const io = require('socket.io')(8082);
 let connections = new Map();
 
 let users = new Map();
-let usersList = new Map();
 
 /**
  * Create the default namespace. This is where the mobile devices connect
@@ -19,12 +18,12 @@ io.on('connection', async (socket) => {
 	});
 
 	// send the user list to the connected device
-	socket.emit('user-list', usersList.map(u => u.name));
+	socket.emit('user-list', Array.from(users.values()));
 
 	socket.on('request-roll', async callbackFn => {
 		console.log(`Socket server - Received request for dice roll from '${socket.id}'`);
 
-		// right now it loops through and emits to all foundry instances. Which while testing is simply 
+		// right now it loops through and emits to all foundry instances. Which while testing is simply
 		// one instance. need to be able to 'connect' the phone app to the identity in game
 		const result = await new Promise(async resolve => {
 			let result = 0;
@@ -49,11 +48,14 @@ foundryNsp.on('connection', socket => {
 	console.log(`Socket server - foundry app '${socket.id}' has connected to server`);
 	foundryConnections.set(socket.id, socket);
 
+	// when a foundry app connects, add that foundry instance's user to the list of users
 	socket.on('add-user', user => {
-		console.log(`Socket server - Received new user ${users}`);
+		console.log(`Socket server - Received new user ${user}`);
 		users.set(socket.id, user);
+
+		// emit the new user list to all phone connections
 		for (const [id, socket] of connections) {
-			socket.emit('user-list', users);
+			socket.emit('user-list', Array.from(users.values()));
 		}
 	});
 
@@ -61,5 +63,10 @@ foundryNsp.on('connection', socket => {
 		console.log(`Socket server - foundry app '${socket.id}' has disconnected from server`);
 		users.delete(socket.id);
 		foundryConnections.delete(socket.id);
+
+		for (const [id, s] of connections) {
+			// send the user list to the connected device
+			s.emit('user-list', Array.from(users.values()));
+		}
 	});
 });
