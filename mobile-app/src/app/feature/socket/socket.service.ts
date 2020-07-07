50059eba57b31a8b1dda6e6d2199033c6b5fc88e
kyle-mcknight <kyle.mcknight@accesso.com>
2020-07-06 23:14:58 -0400

    if the host changes, disconnect from current socket, create a new one with the new host and connect to it.

--- mobile-app/src/app/feature/socket/socket.service.ts
+++ mobile-app/src/app/feature/socket/socket.service.ts
@@ -1,46 +1,93 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { Inject, Injectable, OnDestroy } from '@angular/core';
+import { select, Store } from '@ngrx/store';
 import { Socket } from 'ngx-socket-io';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Subject, Observable } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
 import { MessageTypes } from 'src/app/message/message-types';
-import { takeUntil, tap } from 'rxjs/operators';
-import { Store } from '@ngrx/store';
-import { FoundryState } from '../foundry/redux/foundry.state';
-import { foundryUserListReceived } from '../foundry/redux/foundry.actions';
 import { User } from '../foundry/foundry.models';
+import { foundryUserListReceived } from '../foundry/redux/foundry.actions';
+import { FoundryState } from '../foundry/redux/foundry.state';
+import { selectSetting } from '../settings/redux/settings.selectors';
+import { SettingsState } from '../settings/redux/settings.state';
+import { COMPANION_SOCKET } from './socket.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService implements OnDestroy {
-  private _destroy$: Subject<void> = new Subject<void>();
-  private _connected$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private _connectedSub$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  public connected$: Observable<boolean> = this._connectedSub$.asObservable();
 
-  public connected$: Observable<boolean> = this._connected$.asObservable();
+  private _destroy$: Subject<void> = new Subject<void>();
+  private _previousHost: string;
 
   constructor(
     private _socket: Socket,
-    private _store: Store<FoundryState>
+    private _foundryStore: Store<FoundryState>,
+    private _settingsStore: Store<SettingsState>,
+    @Inject(COMPANION_SOCKET) private _companionSocket: BehaviorSubject<Socket>
   ) {
-    this._socket.on('connect', this.socketConnected);
-    this._socket.on('disconnect', this.socketDisconnected);
+    /**
+     * this is a little confusing. I wanted to handle the scenario where a user will change
+     * the host url. I couldn't find anyway in the socket io module to change the url. So instead,
+     * I created an injection token named COMPANION_SOCKET. This injection token will inject a j
+     * BehaviorSubject<Socket> instnance. When first creating the SocketService, we get a handle
+     * to the Socket instance provided the socket module that's created from teh SocketIOModule.forRoot method
+     * in AppModule and we send that in the BehaviorSubject. When the host changes, we disconnect the Socket
+     * instance and create a brand new one. This new socket is 'nexted' in the BehaviorSubject associated
+     * with the InjectionToken. Anyone that wants the current instance of Socket beingused should inject
+     * the COMPANION_SOCKET and NOT the normal Socket
+     */
+    this._companionSocket.next(_socket);
+
+    this._socket.once('connect', this.socketConnected);
+    this._socket.once('disconnect', this.socketDisconnected);
+
+    this._settingsStore.pipe(
+      select(selectSetting('host')),
+      take(1)
+    ).subscribe(value => {
+      this._previousHost = value;
+    });
+
+    this._settingsStore.pipe(select(selectSetting('host')), takeUntil(this._destroy$)).subscribe(this.handleHostChanged);
 
     // observable to obtain the up-to-date users list when the server sends it
+    // TODO: should this be moved elsewhere? would all consumers of socket service care about this?
     this._socket.fromEvent<User[]>(MessageTypes.USER_LIST).pipe(
       takeUntil(this._destroy$)
     ).subscribe(users => {
-      this._store.dispatch(foundryUserListReceived({ users }));
+      this._foundryStore.dispatch(foundryUserListReceived({ users }));
     });
+  }
+
+  private handleHostChanged = (newHost: string): void => {
+    if (newHost === this._previousHost) {
+      return;
+    }
+
+    console.log('disconnecting old socket');
+    this._socket.disconnect();
+
+    this._socket = new Socket({
+      url: `${newHost}/mobile`
+    });
+
+    this._previousHost = newHost;
+    this._socket.once('connect', this.socketConnected);
+    this._socket.once('disonnect', this.socketDisconnected);
+    this._socket.connect();
   }
 
   private socketConnected = (): void => {
     console.log('Connected to socket server');
-    this._connected$.next(true);
     void this.emit(MessageTypes.REQUEST_USER_LIST);
+    this._connectedSub$.next(true);
   }
 
   private socketDisconnected = (reason: string): void => {
     console.log(`Diconnected from socket server. '${reason}'`);
-    this._connected$.next(false);
+    this._connectedSub$.next(false);
   }
 
   public async emit<T>(eventName: MessageTypes, callback: boolean = false, ...args: any[]): Promise<T> {
